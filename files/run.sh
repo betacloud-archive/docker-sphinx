035c63f36c456180c7e59e2e8bed9b3ee63180ac
Christian Berendt <berendt@betacloud-solutions.de>
2018-09-24 23:05:35 +0200

    Fix wrong variable name

--- files/run.sh
+++ files/run.sh
@@ -30,6 +30,6 @@ if [[ $TYPE == "latex" ]]; then
     cd /latex
     LATEXMKOPTS="-interaction=nonstopmode -f" make -e all-pdf
 
-    cp /latex/${FILE%.rst}.pdf /pdf/$(date +%Y%m%)-${FILE%.rst}.pdf
+    cp /latex/${FILENAME%.rst}.pdf /pdf/$(date +%Y%m%)-${FILENAME%.rst}.pdf
 
 fi
